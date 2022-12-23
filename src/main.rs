1194fa683e719c246aae500a8128260f0ea6d920
Patrick Mueller <patrick-mueller@live.de>
2022-12-23 08:13:17 +0000

    feat: cli with clap (verbose, help, version, temperature-poll-ms and temperature-spi)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[clap(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
+
+    // Temperature Poll Intervall
+    #[arg(value_parser = parse_duration, long, default_value = "100")]
+    temperature_poll_ms: std::time::Duration,
+
+    // Temperature SPI Interface
+    #[arg(long, default_value = "/dev/spidev0.0")]
+    temperature_spi: String,
+}
+
+fn parse_duration(arg: &str) -> Result<std::time::Duration, std::num::ParseIntError> {
+    let ms = arg.parse()?;
+    Ok(std::time::Duration::from_millis(ms))
+}
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
 }
